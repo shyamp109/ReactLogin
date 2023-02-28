0436b99a6b62a6c9fc8269b4360f7b3358fa19f9
IPS-162 <ips-162@IPS-162s-iMac.local>
2023-02-28 12:13:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import logo from './hero-logo 1.png'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='d-flex justify-content-center align-items-center vh-100'>
+        <div className='main-login-page'>
+          <div className='d-flex justify-content-between align-items-center'>
+            <div className='d-flex justify-content-center flex-column'>
+              <h3 className='text-login'>Login</h3>
+              <p className='text-login-bottom'>Please Enter Your Cred</p>
+            </div>
+            <img src={logo} alt="logo" />
+          </div>
+          <div className='d-flex flex-column justify-content-center gap-2'>
+            <input className='input-email' type="text" placeholder='Email' />
+            <input className='input-password' type="password" placeholder='Password' />
+          </div>
+          <div className="d-flex justify-content-end align-items-center my-3">
+            <a className='text-forgot text-decoration-none' href="#" >Forgot Password?</a>
+          </div>
+          
+          
+          
+            <button className='btn-login'>Login</button>
+          
+          <div className='d-flex justify-content-center  mt-5'>
+            <p className='text-login-bottom'>Don’t have an account ? <a className='text-login-bottom text-decoration-none' href="#" >Signup</a></p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
